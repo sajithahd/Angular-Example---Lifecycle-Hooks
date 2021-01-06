8b9c5092b73f19b00e5de1d81a8cd7afe72d83e0
sajithahd <hdsajitha@gmail.com>
2021-01-06 22:57:45 +0530

    5 files modified, 1 added

---
+++ src/app/pick-a-boo-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'pick-a-boo-wrapper',
+  template: `
+    <div>
+      Pick A Boo Component
+    </div>
+  `,
+  styleUrls: [ './app.component.css' ]
+})
+
+export class pickABooWrapperComponent{
+
+}
